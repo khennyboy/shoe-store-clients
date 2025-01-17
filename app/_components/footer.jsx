4cd40c768b2f30419556763d0bfef411958e8970
khennyboy <abdullateefkehinde848@gmail.com>
2025-01-17 19:06:53 +0200

    done

--- app/_components/footer.jsx
+++ app/_components/footer.jsx
@@ -1,45 +1,69 @@
+"use client";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+
+const navLinks = [
+  { name: "Collections", path: "/" },
+  { name: "Men", path: "/?filter=men" },
+  { name: "Women", path: "/?filter=women" },
+  { name: "About", path: "/about" },
+  { name: "Contact", path: "/contact" },
+];
+
 export default function Footer() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const isFilter = searchParams.get("filter");
+
   return (
-    <footer className="bg-gray-700 py-10 text-white">
-      <div className="container mx-auto grid grid-cols-1 gap-8 md:grid-cols-3">
-        <div>
-          <h2 className="mb-4 text-xl font-bold">Sole Mate</h2>
+    <footer className="bg-gray-700 px-4 py-12 text-white md:px-8 xl:px-12">
+      <div className="grid grid-cols-1 grid-rows-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
+        <div className="pr-4">
+          <h2 className="mb-2 text-lg font-bold md:mb-4 md:text-xl">
+            Sole Mate
+          </h2>
           <p className="text-gray-400">
             Step into comfort and style. Find your perfect fit with us.
           </p>
         </div>
 
-        <div>
-          <h3 className="mb-4 text-lg font-semibold">Quick Links</h3>
-          <ul className="space-y-2">
-            <li>
-              <a href="#" className="hover:text-gray-300">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-300">
-                Shop
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-300">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-300">
-                Contact
-              </a>
-            </li>
+        <div className="">
+          <h3 className="mb-2 font-semibold md:mb-4 md:text-lg">Quick Links</h3>
+          <ul className="space-y-1">
+            {navLinks.map((each, index) => {
+              const isFilter = searchParams.get("filter");
+              const isActive =
+                (each.path === pathname && each.name !== "Collections") ||
+                (each.name === "Collections" &&
+                  isFilter === null &&
+                  each.path === pathname) ||
+                (each.name === "Men" && isFilter === "men") ||
+                (each.name === "Women" && isFilter === "women");
+              return (
+                <li key={index}>
+                  <Link
+                    href={each.path}
+                    className={`transition-all duration-200 ease-linear hover:opacity-100 ${isActive ? "opacity-100" : "opacity-70"}`}
+                  >
+                    {each.name}
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
         </div>
 
-        <div>
-          <h3 className="mb-4 text-lg font-semibold">Contact Us</h3>
-          <p className="text-gray-400">123 Shoe Lane, Fashion City</p>
-          <p className="text-gray-400">Email: support@shoehaven.com</p>
-          <p className="text-gray-400">Phone: +123 456 7890</p>
+        <div className="col-span-full md:col-span-1">
+          <h3 className="mb-2 font-semibold md:mb-4 md:text-lg">Contact Us</h3>
+          <p className="text-gray-400">
+            1, ola-oluwa street papa ashafa Agege, Lagos State
+          </p>
+          <p className="text-gray-400">
+            <a href="mailto:abdullateefkehinde848@gmail.com">
+              abdullateefkehinde848@gmail.com
+            </a>
+          </p>
+          <p className="text-gray-400">Phone: +234 702 6771 744</p>
 
           <div className="mt-4 flex space-x-4">
             <a href="#" className="text-gray-400 hover:text-gray-300">
@@ -55,9 +79,9 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="mt-8 border-t border-gray-700 pt-4 text-center">
+      <div className="mt-4 text-center sm:mt-8">
         <p className="text-sm text-gray-400">
-          &copy; 2025 Shoe Haven. All Rights Reserved.
+          &copy; 2025 Sole Mate. All Rights Reserved.
         </p>
       </div>
     </footer>
--- app/page.jsx
+++ app/page.jsx
@@ -43,7 +43,7 @@ export default function Home() {
     );
   if (isError) {
     return (
-      <div className="relative rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700">
+      <div className="ujujjhyhyhjrounded border border-red-400 bg-red-100 px-4 py-3 text-red-700">
         {error.message}
       </div>
     );
